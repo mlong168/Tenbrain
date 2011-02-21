a58855af88e26f8172f6f6446fb4b591b52c98f9
(no author) <(no author)@8630e248-272e-11e0-8c07-d7910d7a0169>
2011-02-21 12:49:11 +0000

    linked accounts page layout changes

--- system/application/modules/account/views/account_linked.php
+++ system/application/modules/account/views/account_linked.php
@@ -21,88 +21,67 @@
 	<?php else :?>
 		<?php if ($facebook_links) : ?>
 			<?php foreach ($facebook_links as $facebook_link) : ?>
-	<div class="grid_1 alpha">
-		<img src="resource/app/img/icon/facebook.png" alt="<?php echo lang('connect_facebook'); ?>" title="<?php echo lang('connect_facebook'); ?>" width="40" />
-	</div>
-	<div class="grid_3">
-		<?php echo lang('connect_facebook'); ?><br />
-		<?php echo anchor('http://facebook.com/profile.php?id='.$facebook_link->facebook_id, substr('http://facebook.com/profile.php?id='.$facebook_link->facebook_id, 0, 30).(strlen('http://facebook.com/profile.php?id='.$facebook_link->facebook_id) > 30 ? '...' : ''), array('target' => '_blank', 'title' => 'http://facebook.com/profile.php?id='.$facebook_link->facebook_id)); ?>
-	</div>
-	<div class="grid_1 omega">
-				<?php if ($num_of_linked_accounts != 1) : ?>
-		<?php echo form_open(uri_string()); ?>
-			<?php echo form_fieldset(); ?>
-				<?php echo form_hidden('facebook_id', $facebook_link->facebook_id); ?>
-				<?php echo form_button(array(
-						'type' => 'submit',
-						'class' => 'button',
-						'content' => lang('linked_remove')
-					)); ?>
-			<?php echo form_fieldset_close(); ?>
-		<?php echo form_close(); ?>
-				<?php endif; ?>
-	</div>
-	<div class="clear"></div>
+				<div class="linked_account">
+					<div class="control third_party big picture facebook"></div>
+					<span class="caption"><?php echo lang('connect_facebook'); ?></span>
+						<?php if ($num_of_linked_accounts != 1) : ?>
+							<?php echo form_open(uri_string(), array('class' => 'remove')); ?>
+								<?php echo form_hidden('facebook_id', $facebook_link->facebook_id); ?>
+								<?php echo form_button(array(
+										'type'		=> 'submit',
+										'name'		=> 'submit',
+										'class'		=> 'login_submit',
+										'content'	=> lang('linked_remove')
+									)); ?>
+							<?php echo form_close(); ?>
+						<?php endif; ?>
+					<?php echo anchor('http://facebook.com/profile.php?id='.$facebook_link->facebook_id, substr('http://facebook.com/profile.php?id='.$facebook_link->facebook_id, 0, 30).(strlen('http://facebook.com/profile.php?id='.$facebook_link->facebook_id) > 30 ? '...' : ''), array('target'	=> '_blank', 'class' => 'linked_link', 'title'	=> 'http://facebook.com/profile.php?id='.$facebook_link->facebook_id)); ?>
+				</div>	
 			<?php endforeach; ?>
 		<?php endif; ?>
 		<?php if ($twitter_links) : ?>
 			<?php foreach ($twitter_links as $twitter_link) : ?>
-	<div class="grid_1 alpha">
-		<img src="resource/app/img/icon/twitter.png" alt="<?php echo lang('connect_twitter'); ?>" title="<?php echo lang('connect_twitter'); ?>" width="40" />
-	</div>
-	<div class="grid_3">
-		<?php echo lang('connect_twitter'); ?><br />
-		<?php echo anchor('http://twitter.com/'.$twitter_link->twitter->screen_name, substr('http://twitter.com/'.$twitter_link->twitter->screen_name, 0, 30).(strlen('http://twitter.com/'.$twitter_link->twitter->screen_name) > 30 ? '...' : ''), array('target' => '_blank', 'title' => 'http://twitter.com/'.$twitter_link->twitter->screen_name)); ?>
-	</div>
-	<div class="grid_1 omega">
-				<?php if ($num_of_linked_accounts != 1) : ?>
-		<?php echo form_open(uri_string()); ?>
-			<?php echo form_fieldset(); ?>
-				<?php echo form_hidden('twitter_id', $twitter_link->twitter_id); ?>
-				<?php echo form_button(array(
-						'type' => 'submit',
-						'class' => 'button',
-						'content' => lang('linked_remove')
-					)); ?>
-			<?php echo form_fieldset_close(); ?>
-		<?php echo form_close(); ?>
-				<?php endif; ?>
-	</div>
-	<div class="clear"></div>
+				<div class="linked_account">
+					<div class="control third_party big picture twitter"></div>
+					<span class="caption"><?php echo lang('connect_twitter'); ?></span>
+						<?php if ($num_of_linked_accounts != 1) : ?>
+							<?php echo form_open(uri_string(), array('class' => 'remove')); ?>
+								<?php echo form_hidden('twitter_id', $twitter_link->twitter_id); ?>
+								<?php echo form_button(array(
+										'type'		=> 'submit',
+										'name'		=> 'submit',
+										'class'		=> 'login_submit',
+										'content'	=> lang('linked_remove')
+									)); ?>
+							<?php echo form_close(); ?>
+						<?php endif; ?>
+					<?php echo anchor('http://twitter.com/'.$twitter_link->twitter->screen_name, substr('http://twitter.com/'.$twitter_link->twitter->screen_name, 0, 30).(strlen('http://twitter.com/'.$twitter_link->twitter->screen_name) > 30 ? '...' : ''), array('target'	=> '_blank', 'class' => 'linked_link', 'title'	=> 'http://twitter.com/'.$twitter_link->twitter->screen_name)); ?>
+				</div>
 			<?php endforeach; ?>
 		<?php endif; ?>
 		<?php if ($openid_links) : ?>
 			<?php foreach ($openid_links as $openid_link) : ?>
-	<div class="grid_1 alpha">
-		<img src="resource/app/img/icon/<?php echo $openid_link->provider; ?>.png" alt="<?php echo lang('connect_'.$openid_link->provider); ?>" width="40" />
-	</div>
-	<div class="grid_3">
-		<?php echo lang('connect_'.$openid_link->provider); ?><br />
-		<?php echo anchor($openid_link->openid, substr($openid_link->openid, 0, 30).(strlen($openid_link->openid) > 30 ? '...' : ''), array('target' => '_blank', 'title' => $openid_link->openid)); ?>
-	</div>
-	<div class="grid_1 omega">
-				<?php if ($num_of_linked_accounts != 1) : ?>
-		<?php echo form_open(uri_string()); ?>
-			<?php echo form_fieldset(); ?>
-				<?php echo form_hidden('openid', $openid_link->openid); ?>
-				<?php echo form_button(array(
-						'type' => 'submit',
-						'class' => 'button',
-						'content' => lang('linked_remove')
-					)); ?>
-			<?php echo form_fieldset_close(); ?>
-		<?php echo form_close(); ?>
-				<?php endif; ?>
-	</div>
-	<div class="clear"></div>
+				<div class="linked_account">
+					<div class="control third_party big picture centered openid"></div>
+					<span class="caption"><?php echo lang('connect_'.$openid_link->provider); ?></span>
+						<?php if ($num_of_linked_accounts != 1) : ?>
+							<?php echo form_open(uri_string(), array('class' => 'centered')); ?>
+								<?php echo form_hidden('openid', $openid_link->openid); ?>
+								<?php echo form_button(array(
+										'type'		=> 'submit',
+										'name'		=> 'submit',
+										'class'		=> 'login_submit',
+										'content'	=> lang('linked_remove')
+									)); ?>
+							<?php echo form_close(); ?>
+						<?php endif; ?>
+					<?php echo anchor($openid_link->openid, substr($openid_link->openid, 0, 30).(strlen($openid_link->openid) > 30 ? '...' : ''), array('target' => '_blank', 'title' => $openid_link->openid, 'class' => 'linked_link')); ?>
+				</div>
 			<?php endforeach; ?>
 		<?php endif; ?>
 	<?php endif; ?>
 </div>
 <h3><?php echo lang('linked_link_with_your_account_from'); ?></h3>
-<?php if ($this->session->flashdata('linked_error')) : ?>
-<div class="form_error"><?php echo $this->session->flashdata('linked_error'); ?></div>
-<?php endif; ?>
 <ul class="third_parties">
 	<?php foreach($this->config->item('third_party_auth_providers') as $provider) : ?>
 	<li class="control third_party big <?php echo $provider; ?>">
@@ -116,5 +95,9 @@
 	</li>
 	<?php endforeach; ?>
 </ul>
+<div class="clearfix"></div>
+<?php if ($this->session->flashdata('linked_error')) : ?>
+<p class="input_wrapper center account_error"><?php echo $this->session->flashdata('linked_error'); ?></p>
+<?php endif; ?>
 
 </body></html>
