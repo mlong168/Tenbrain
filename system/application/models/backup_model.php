4b3c68c89ed9fe2560b2818938ce30259a9d2ca3
atsok <atsok@softjourn.com>
2011-04-27 18:36:22 +0300

    Reverted get_backup_details

--- system/application/models/backup_model.php
+++ system/application/models/backup_model.php
@@ -82,4 +82,29 @@ class Backup_model extends Model {
 		}
 		return $result;
 	}
+	
+	public function get_backup_details($backup_ids, $fields = array('backup_name'))
+	{
+		 $possible_fields = array('backup_id', 'provider_backup_id', 'backup_name', 'provider', 'description', 'created_on', 'instance_id');
+		 $fields_to_retrieve = array();
+		 if(!is_array($fields)) $fields = array($fields);
+		 foreach($fields as $field)
+		 {
+		  if(in_array($field, $possible_fields)) $fields_to_retrieve []= $field;
+		 }
+		 if($fields === array('*')) $fields_to_retrieve = $possible_fields; // select all
+		 foreach($fields_to_retrieve as &$field) $field = 'ub.' . $field;
+		 
+		 if(!is_array($backup_ids)) $backup_ids = array($backup_ids);
+		 foreach($backup_ids as &$bkps) $bkps = $this->db->escape($bkps);
+		 
+		 $sql = 'SELECT ' . implode(',', $fields_to_retrieve);
+		 $sql .= ' FROM user_backups ub';
+		 $sql .= ' LEFT JOIN user_deleted_backups udb USING(backup_id)';
+		 $sql .= ' WHERE ub.account_id = ' . $this->session->userdata('account_id');
+		 $sql .= ' AND udb.backup_id IS NULL';
+		 $sql .= ' AND ub.backup_id IN(' . implode(',', $backup_ids) . ')';
+		 $query = $this->db->query($sql);
+		 return $query->num_rows() ? $query->result() : array();
+	}
 }
