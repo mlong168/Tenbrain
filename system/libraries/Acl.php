5ce2ee5e7ef068b1ef1e39a00aacc93a1215540a
ydmytriv <ydmytriv@softjourn.com>
2011-05-04 16:24:53 +0300

    changed structure of the acl library

--- system/libraries/Acl.php
+++ system/libraries/Acl.php
@@ -4,6 +4,7 @@ class Acl
 {
 	// Set the instance variable
 	private $CI;
+	private $role = 0;
 
 	function __construct()
 	{
@@ -11,90 +12,84 @@ class Acl
 		$this->CI =& get_instance();
 
 		// Set the include path and require the needed files
-		set_include_path(get_include_path() . PATH_SEPARATOR . BASEPATH . "application/libraries");
+		set_include_path(get_include_path() . PATH_SEPARATOR . BASEPATH . 'application/libraries');
 		
 		require_once(APPPATH . '/libraries/Zend/Acl.php');
 		require_once(APPPATH . '/libraries/Zend/Acl/Role.php');
 		require_once(APPPATH . '/libraries/Zend/Acl/Resource.php');
 		
-		$this->zacl = new Zend_Acl();
-
-		// Set the default ACL
-		$this->zacl->addRole(new Zend_Acl_Role('default'));
-		$query = $this->CI->db->get('account_aclresources');
-		foreach($query->result() AS $row)
+		// get the role for current user
+		$this->CI->db->select('roleid');
+		$query = $this->CI->db->get_where('a3m_account', array('id' => $this->CI->session->userdata('account_id')));
+		if($query->num_rows())
 		{
-			$this->zacl->add(new Zend_Acl_Resource($row->resource));
-			if($row->default_value == 'true')
-			{
-				$this->zacl->allow('default', $row->resource);
-			}
+			$this->role = $query->row()->roleid;
 		}
-		// Get the ACL for the roles
-		$this->CI->db->order_by("roleorder", "ASC");
-		$query = $this->CI->db->get('account_aclroles');
-		foreach($query->result() AS $row)
+
+		$this->acl = new Zend_Acl();
+ 
+		$this->CI->db->order_by('ParentId', 'ASC'); //Get the roles
+		$query = $this->CI->db->get('user_roles');
+		$roles = $query->result();
+ 
+		$this->CI->db->order_by('parentId', 'ASC'); //Get the resources
+		$query = $this->CI->db->get('user_resources');
+		$resources = $query->result();
+ 
+		$query = $this->CI->db->get('user_permissions'); //Get the permissions
+		$permissions = $query->result();
+
+		//Add the roles to the ACL
+		foreach ($roles as $roles)
 		{
-			$role = (string)$row->name;
-			$this->zacl->addRole(new Zend_Acl_Role($role), 'default');
-			$this->CI->db->from('account_acl');
-			$this->CI->db->join('account_aclresources', 'account_acl.resource_id = account_aclresources.id');
-			$this->CI->db->where('type', 'role');
-			$this->CI->db->where('type_id', $row->id);
-			
-			$subquery = $this->CI->db->get();
-			foreach($subquery->result() AS $subrow)
-			{
-				if($subrow->action === 'allow')
-				{
-					$this->zacl->allow($role, $subrow->resource);
-				}
-				else
-				{
-					$this->zacl->deny($role, $subrow->resource);
-				}
-			}
-			
-			// Get the ACL for the users
-			$this->CI->db->from('a3m_account');
-			$this->CI->db->where('roleid', $row->id);
-			$userquery = $this->CI->db->get();
-			
-			foreach($userquery->result() AS $userrow)
-			{
-				$this->zacl->addRole(new Zend_Acl_Role($userrow->username), $role);
-				$this->CI->db->from('account_acl');
-				$this->CI->db->join('account_aclresources', 'account_acl.resource_id = account_aclresources.id');
-				$this->CI->db->where('type', 'user');
-				$this->CI->db->where('type_id', $userrow->id);
-				$usersubquery = $this->CI->db->get();
-				foreach($usersubquery->result() AS $usersubrow)
-				{
-					if($usersubrow->action == "allow")
-					{
-						$this->zacl->allow($userrow->username, $usersubrow->resource);
-					}
-					else
-					{
-						$this->zacl->deny($userrow->username, $usersubrow->resource);
-					}
-				}
-			}
+			$role = new Zend_Acl_Role($roles->id);
+			$roles->parentId != null ?
+				$this->acl->addRole($role,$roles->parentId): 
+				$this->acl->addRole($role);
+		}
+
+		//Add the resources to the ACL
+		foreach($resources as $resources)
+		{
+			$resource = new Zend_Acl_Resource($resources->id);
+			$resources->parentId != null ?
+				$this->acl->add($resource, $resources->parentId):
+				$this->acl->add($resource);
+		}
+
+		//Add the permissions to the ACL
+		foreach($permissions as $perms)
+		{
+			$perms->read == '1' ? 
+				$this->acl->allow($perms->role, $perms->resource, 'read') : 
+				$this->acl->deny($perms->role, $perms->resource, 'read');
+			$perms->write == '1' ? 
+				$this->acl->allow($perms->role, $perms->resource, 'write') : 
+				$this->acl->deny($perms->role, $perms->resource, 'write');
+			$perms->modify == '1' ? 
+				$this->acl->allow($perms->role, $perms->resource, 'modify') : 
+				$this->acl->deny($perms->role, $perms->resource, 'modify');
+			$perms->publish == '1' ? 
+				$this->acl->allow($perms->role, $perms->resource, 'publish') : 
+				$this->acl->deny($perms->role, $perms->resource, 'publish');
+			$perms->delete == '1' ? 
+				$this->acl->allow($perms->role, $perms->resource, 'delete') : 
+				$this->acl->deny($perms->role, $perms->resource, 'delete');
 		}
 	}
 
 	// Function to check if the current or a preset role has access to a resource
 	function check_acl($resource, $role = '')
 	{
-		if(!$this->zacl->has($resource))
+		if(!$this->acl->has($resource))
 		{
 			return true;
 		}
 		if(empty($role))
 		{
-			if(isset($this->CI->session->userdata['user']))
+			if(isset($this->role))
 			{
-				$role = $this->CI->session->userdata['user'];
+				$role = $this->role;
 			}
 		}
 		if(empty($role))
@@ -102,5 +97,30 @@ class Acl
 			return false;
 		}
 		return $this->zacl->isAllowed($role, $resource);
+	}
+	
+	function can_read($role, $resource)
+	{
+		return $this->acl->isAllowed($role, $resource, 'read')? TRUE : FALSE;
+	}
+	
+	function can_write($role, $resource)
+	{
+		return $this->acl->isAllowed($role, $resource, 'write')? TRUE : FALSE;
+	}
+	
+	function can_modify($role, $resource)
+	{
+		return $this->acl->isAllowed($role, $resource, 'modify')? TRUE : FALSE;
+	}
+	
+	function can_delete($role, $resource)
+	{
+		return $this->acl->isAllowed($role, $resource, 'delete')? TRUE : FALSE;
+	}
+	
+	function can_publish($role, $resource)
+	{
+		return $this->acl->isAllowed($role, $resource, 'publish')? TRUE : FALSE;
 	}
 }
