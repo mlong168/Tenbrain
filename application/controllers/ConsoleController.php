7a68e0c6349df7b20d687fd1d1ecf4b0eeeff304
mlong168 <mlong@tenbrain.com>
2011-05-17 12:29:08 +0300

    tenbrain console rewritten to zend

---
+++ application/controllers/ConsoleController.php
@@ -0,0 +1,39 @@
+<?php
+
+class ConsoleController extends Zend_Controller_Action
+{
+
+	public function init()
+	{
+		/* set alternate layout */
+		$helper = $this->_helper->getHelper('Layout');
+		$layout = $helper->getLayoutInstance();
+		
+		$layout->setLayout('control_panel');
+		$this->view->headTitle()->prepend('Tenbrain Control Panel');
+		
+		$scripts = array(
+			'extjs4/ext-all-debug',
+			'cp/instances',
+			'cp/images',
+			'cp/snapshots',
+			'cp/profile',
+			'cp/transferer'
+		);
+		
+		// if($this->account_type === 'premium') $scripts = array_merge($scripts, array('cp/load_balancers', 'cp/elastic_ips'));
+		
+		$scripts []= 'cp/cp';
+		
+		foreach($scripts as $script)
+		{
+			$this->view->headScript()->appendFile("/js/{$script}.js", 'text/javascript');
+		}
+	}
+	
+	public function indexAction()
+	{
+		
+	}
+
+}
