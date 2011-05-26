9b5749619fae6734fd960a324bfb68021670e394
atsok <atsok@softjourn.com>
2011-05-26 21:44:44 +0300

    Added functionality for working with cassandra for servers (instances)

--- application/models/Servers.php
+++ application/models/Servers.php
@@ -2,29 +2,139 @@
 
 class Application_Model_Servers
 {
+	private $user_id;
 	private $cassie;
-	private $auth;
-	
-	function __construct()
+
+	function __construct ()
 	{
-		$this->cassie = new ZendExt_Cassandra("SERVERS");
-		$this->auth = Zend_Auth::getInstance();
+		$this->user_id = Zend_Auth::getInstance()->getIdentity()->id;
+		$this->cassie = new ZendExt_Cassandra();
+	}
+
+	public function addServers (array $servers)
+	{
+		$this->cassie->useColumnFamilies(array('SERVERS', 'USER_SERVERS'));
+		
+		foreach ($servers as $data) {
+			$uuid = ZendExt_CassandraUtil::uuid1();
+			$data['server_id'] = $uuid;
+			$this->cassie->SERVERS->insert($uuid, $data);
+			$this->cassie->USER_SERVERS->insert($this->user_id, 
+			array($uuid => ''));
+		}
+	}
+
+	public function removeServers (array $server_ids)
+	{
+		$this->cassie->useColumnFamilies('USER_SERVERS', 'USER_SERVERS');
+		
+		foreach ($server_ids as $id)
+			$this->cassie->USER_DELETED_SERVERS->insert($this->user_id, 
+			array($id => ''));
+		
+		$this->cassie->USER_SERVERS->remove($this->user_id, $server_ids);
+	}
+
+	public function getUserServers ()
+	{
+		$servers = array();
+		$this->cassie->useColumnFamilies(array('SERVERS', 'USER_SERVERS'));
+		
+		$server_ids = $this->cassie->USER_SERVERS->get($this->user_id);
+		foreach ($server_ids as $id) {
+			$servers[] = $this->cassie->SERVERS->get($id);
+		}
+		return $servers;
+	}
+
+	public function getUserTerminatedServers ()
+	{
+		$servers = array();
+		$this->cassie->useColumnFamilies(
+		array('SERVERS', 'USER_DELETED_SERVERS'));
+		
+		$server_ids = $this->cassie->USER_DELETED_SERVERS->get($this->user_id);
+		foreach ($server_ids as $id) {
+			$servers[] = $this->cassie->SERVERS->get($id);
+		}
+		return $servers;
+	}
+
+	public function getServersDetails ($instance_ids, $fields = array('instance_name'))
+	{
+		$servers = array();
+		$possible_fields = array('server_id', 'provider_server_id', 'server_name', 'provider', 'public_ip', 'created_on');
+		$fields_to_retrieve = array();
+		if (! is_array($fields)) $fields = array($fields);
+		foreach ($fields as $field) {
+			if (in_array($field, $possible_fields)) $fields_to_retrieve[] = $field;
+		}
+		if ($fields === array('*')) $fields_to_retrieve = $possible_fields;
+		$columns = implode(',', $fields_to_retrieve);
+		
+		$this->cassie->useColumnFamilies(array('SERVERS', 'USER_SERVERS'));
+		
+		$server_ids = $this->cassie->USER_SERVERS->get($this->user_id);
+		foreach ($server_ids as $id) {
+			$servers[] = $this->cassie->SERVERS->get($id, $columns);
+		}
+		return $servers;
 	}
 	
-	public function add_server(array $data)
+	public function get_server_ids($provider_server_ids)
 	{
-		$uuid = ZendExt_CassandraUtil::uuid1();
+		$server_ids = array();
+		if(!is_array($provider_instance_ids)) $provider_instance_ids = array($provider_instance_ids);
+		$user_servers = $this->getUserServers();
+		foreach ($servers as $server)
+		{
+			if(in_array($server['provider_server_id'], $provider_instance_ids))
+				$server_ids[] = $server['server_id'];
+		}
+		return $server_ids;
+	}
+	
+	public function remove_servers_in_lb($load_balancer_id, $server_ids)
+	{
+		$this->cassie->useColumnFamilies(array('USER_LOADBALLANCERS', 'USER_LOADBALLANCER_SERVERS'));
+		$lb = $this->cassie->USER_LOADBALLANCERS->get($this->user_id, $load_balancer_id); // Check if user have this LB
+		if($lb)
+			$this->cassie->USER_LOADBALLANCER_SERVERS->remove($load_balancer_id, $server_ids);
+	}
+	
+	public function add_servers_in_lb($load_balancer_id, $server_ids)
+	{
+		$this->cassie->useColumnFamilies(array('USER_LOADBALLANCERS, USER_LOADBALLANCER_SERVERS'));
+		$lb = $this->cassie->USER_LOADBALLANCERS->get($this->user_id, $load_balancer_id); // Check if user have this LB
+		if($lb)
+			$this->cassie->USER_LOADBALLANCER_SERVERS->insert($load_balancer_id, $server_ids);
+	}
+	
+	public function get_servers_available_for_lb($provider, $load_balancer_id)
+	{
+		$this->cassie->useColumnFamilies(array('USER_LOADBALLANCERS, USER_SERVERS_LOADBALLANCER'));
 		
-		$this->cassie->insert($uuid, $data);
+		$lb = $this->cassie->USER_LOADBALLANCERS->get($this->user_id, $load_balancer_id);
+		if(!$lb)
+			return array();
 		
-		$this->cassie->set_column_family('USER_SERVERS');
-		
-		$my_server_ids = $cassie->get($this->auth->getIdentity()->id);
-		$my_server_ids = explode(',', $my_server_ids['server_ids']);
-		$my_server_ids = array_merge($my_server_ids, array($uuid));
-		
-		$this->cassie->insert($this->auth->getIdentity()->id, array(
-			'server_ids' => implode(',', $my_server_ids)
-		));
+		$lb_servers = (array) $this->cassie->USER_LOADBALLANCER_SERVERS->get($load_balancer_id);
+		$user_servers = $this->getUserServers();
+
+		$servers = array();
+		if($user_servers)
+		{
+			foreach($user_servers as $user_server)
+			{
+				if(!in_array($lb_servers, $user_servers))
+					$servers[] = array(
+						'server_id'				=> $user_server['server_id'],
+						'provider_server_id'	=> $user_server['provider_server_id'],
+						'name'					=> $user_server['server_name'],
+						'ip_address'			=> $user_server['public_ip']
+					);
+			}
+		}
+		return $servers;
 	}
 }
--- library/ZendExt/Cassandra.php
+++ library/ZendExt/Cassandra.php
@@ -3,13 +3,10 @@ require_once 'ZendExt/Cassandra/columnfamily.php';
 
 class ZendExt_Cassandra 
 {
-	
 	const KEY_SPACE = 'Tenbrain_dev';
 	const SERVER = '50.19.88.0:9160';
 	
-	public $column_families;
-	
-	public function use_column_families(array $families)
+	public function useColumnFamilies(array $families)
 	{
 		$pool = new ConnectionPool(self::KEY_SPACE, array(self::SERVER));
 		foreach($families as $family)
@@ -17,5 +14,4 @@ class ZendExt_Cassandra
 			$this->$family = new ColumnFamily($pool, $family);
 		}
 	}
-	
 }
