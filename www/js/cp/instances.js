9a8297f70aa66c51adf6864c150256e52c9353d1
mlong168 <mlong@tenbrain.com>
2011-06-08 19:29:37 +0300

    web console multiple iframes bug fixed removed styles for not used extjs plugin

--- www/js/cp/instances.js
+++ www/js/cp/instances.js
@@ -240,7 +240,7 @@ var Instances = function(){
 	});
 	
 	
-	var instances_menu = new Ext.menu.Menu({
+	var instances_menu = Ext.create('Ext.menu.Menu', {
 		id: 'running_instances_menu',
 		items: [{
 			text: 'Connect',
@@ -273,11 +273,7 @@ var Instances = function(){
 									+ '&provider=' + params.provider
 									+ '&login_user=' + params.login_user
 									+ '&key_name=' + params.username || 'default';
-								if(iframes.getCount() !== 0)
-								{
-									iframes.get(0).destroy;
-									iframes.removeAll();
-								}
+								if(iframes.getCount() !== 0) iframes.clear();
 								iframes.add(Ext.create('Ext.Component', {
 									autoEl: {
 										tag: 'iframe',
@@ -463,7 +459,7 @@ var Instances = function(){
 					}
 				},
 				columns: [
-					{text: "Name", dataIndex: 'name', width: 150, renderer: function(value, metadata, record){
+					{text: "Name", dataIndex: 'name', width: 200, renderer: function(value, metadata, record){
 						if(record.data.state !== 'running') metadata.css = 'grid-loader';
 						return value;
 					}},
