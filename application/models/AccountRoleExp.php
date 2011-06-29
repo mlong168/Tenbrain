1b521314b106f928c086d7f121417874dcbbfe68
Oleh <alekhandro3@mail.ru>
2011-06-29 17:10:20 +0300

    added some methods to check role expiration

--- application/models/AccountRoleExp.php
+++ application/models/AccountRoleExp.php
@@ -12,4 +12,16 @@ class Application_Model_AccountRoleExp extends Zend_Db_Table_Abstract
 	 */
 	protected $_name = 'account_role_exp';
 	
+	public function isExpired($user_id, $role_id){
+		$sql = $this->_db->select()
+						->from($this->_name)
+						->where('account_id = ?', $user_id)
+						->where('role_id = ?', $role_id)
+						->where('expiration_date >= CURDATE()');
+		$result = $this->_db->query($sql)->fetch();
+		if($result)
+			return false;
+		else
+			return true;
+	}		
 }
